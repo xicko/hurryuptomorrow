1b03e052d4743b75f9ab6a0e84c49c3eeee5f817
xicko <gfieldmn@gmail.com>
2024-11-28 21:47:12 +1100

    added red text glow effect, underlined footer text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import Countdown from "./components/Countdown";
 
 export default function Home() {
   return (
-    <section className="grid justify-center place-content-center md:gap-y-20 gap-y-8 w-full h-[100dvh] text-[#c01101] select-none">
+    <section className="grid justify-center place-content-center md:gap-y-20 gap-y-8 w-full h-[100dvh] text-[#c01101] animate-subtleGlow select-none">
       <p className="md:text-9xl text-6xl text-center">
         HURRY
         <br />
@@ -14,8 +15,7 @@ export default function Home() {
       <Countdown date={`2025-01-24T00:00:00`} />
 
       <div className="fixed font-[Helvetica] font-normal text-sm bottom-0 px-4 py-4">
-        All media content, images, and assets are the property of The Weeknd and
-        XO Records.
+        All media content, images, and assets are the property of <Link href="https://theweeknd.com/" target="_blank"><span className='underline'>The Weeknd</span> and <span className='underline'>XO Records</span>.</Link>
         <br />
         No copyright infringement is intended.
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,21 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        subtleGlow: {
+          '0%, 100%': {
+            textShadow: '0 0 8px rgba(192, 17, 1, 0.5), 0 0 12px rgba(192, 17, 1, 0.4), 0 0 18px rgba(192, 17, 1, 0.3)',
+            
+          },
+          '50%': {
+            textShadow: '0 0 4px rgba(192, 17, 1, 0.3), 0 0 8px rgba(192, 17, 1, 0.2), 0 0 12px rgba(192, 17, 1, 0.1)',
+            
+          },
+        },
+      },
+      animation: {
+        subtleGlow: 'subtleGlow 5s infinite ease-in-out',
+      },
     },
   },
   plugins: [],
